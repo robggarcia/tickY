e9923d61cd3b708c83cf61627644fff40c7c6e52
Rob Garcia <robgarcia@Robs-MacBook-Air.local>
2022-11-01 21:21:18 -0400

    grace starter start

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,31 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Users from "./components/Users";
 
 function App() {
-  return <div className="App">grace starter</div>;
+  const [users, setUsers] = useState([]);
+
+  // i want to fetch the actual books and save them in state
+  const fetchUsers = async () => {
+    const response = await fetch("/api/users");
+    const data = await response.json();
+    console.log(data);
+    setUsers(data.users);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="App">
+      <Link to="/users">Users</Link>
+      <Routes>
+        <Route path="/users" element={<Users users={users} />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
