580685c3a4d3c78a296de037744e9ba9409339f0
robggarcia <robert.g.garcia82@gmail.com>
2022-11-07 08:09:06 -0500

    tests for venues created

---
+++ tests/db/venues.spec.js
@@ -0,0 +1,94 @@
+require("dotenv").config();
+const faker = require("faker");
+const client = require("../../db");
+const { getVenues, createVenue } = require("../../db/venues");
+const { createFakeVenue } = require("../helpers");
+
+describe("DB Venues", () => {
+  describe("getVenues", () => {
+    it("selects and returns an array of all venues", async () => {
+      await createFakeVenue();
+      const venues = await getVenues();
+      const { rows: venuesFromDatabase } = await client.query(`
+            SELECT * FROM venues;`);
+      expect(venues).toEqual(venuesFromDatabase);
+    });
+  });
+
+  describe("getVenuesById", () => {
+    it("gets venues by their id", async () => {
+      const fakeVenue = {
+        name: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        capacity: faker.datatype.number(1000),
+      };
+
+      const venue = await getVenueById(fakeVenue.id);
+
+      expect(venue.id).toEqual(fakeVenue.id);
+      expect(venue.name).toEqual(fakeVenue.name);
+      expect(venue.description).toEqual(fakeVenue.description);
+    });
+  });
+
+  describe("getVenueByName", () => {
+    it("gets an venue by their name", async () => {
+      const fakeVenue = {
+        name: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        capacity: faker.datatype.number(1000),
+      };
+      const venue = await getArtistByName(fakeVenue.name);
+      expect(venue.id).toEqual(fakeVenue.id);
+    });
+  });
+
+  describe("createVenue", () => {
+    it("Creates and returns the new venue", async () => {
+      const venueToCreate = {
+        name: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        capacity: faker.datatype.number(1000),
+      };
+      const createdVenue = await createVenue(venueToCreate);
+      expect(createdVenue.name).toBe(venueToCreate.name);
+      expect(createdVenue.description).toBe(venueToCreate.description);
+    });
+  });
+
+  describe("updateVenue", () => {
+    it("Updates name without affecting the ID. Returns the updated Venue.", async () => {
+      const fakeVenue = await createfakeVenue({
+        name: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        capacity: faker.datatype.number(1000),
+      });
+      const name = "Silly Bumpkins";
+      const updatedVenue = await updateVenue({ id: fakeVenue.id, name });
+      expect(updatedVenue.id).toEqual(fakeVenue.id);
+      expect(updatedVenue.name).toEqual(name);
+      expect(updatedVenue.description).toEqual(fakeVenue.description);
+    });
+
+    it("Updates description without affecting the ID. Returns the updated Venue.", async () => {
+      const fakeVenue = await createfakeVenue({
+        name: faker.address.streetName(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        capacity: faker.datatype.number(1000),
+      });
+      const description = "Kings of Grunge!";
+      const updatedVenue = await updateVenue({
+        id: fakeVenue.id,
+        description,
+      });
+      expect(updatedVenue.id).toEqual(fakeVenue.id);
+      expect(updatedVenue.name).toEqual(fakeVenue.name);
+      expect(updatedVenue.description).toEqual(description);
+    });
+  });
+});
--- tests/helpers.js
+++ tests/helpers.js
@@ -1,6 +1,7 @@
 const faker = require("faker");
 const { createArtist } = require("../db/artists");
 const { createTickets } = require("../db/tickets");
+const { createVenue } = require("../db/venues");
 
 const createFakeArtist = async (
   name = faker.name.findName(),
@@ -14,6 +15,20 @@ const createFakeArtist = async (
     throw new Error("createArtists didn't return a artist");
   }
   return artist;
+};
+
+const createFakeVenue = async (
+  name = faker.address.streetName(),
+  city = faker.address.city(),
+  state = faker.address.state(),
+  capacity = faker.datatype.number(1000)
+) => {
+  const venue = await createVenue({ name, city, state, capacity });
+  console.log("FAKE VENUE CREATED: ", venue);
+  if (!venue) {
+    throw new Error("createVenue didn't return a venue");
+  }
+  return venue;
 };
 
 const createFakeTicket = async () => {
@@ -33,4 +48,4 @@ const createFakeTicket = async () => {
   return ticket;
 };
 
-module.exports = { createFakeArtist, createFakeTicket };
+module.exports = { createFakeArtist, createFakeTicket, createFakeVenue };
