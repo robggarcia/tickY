dbdab7f060606fa60067424cd12c3ac78c1930ed
robggarcia <robert.g.garcia82@gmail.com>
2022-11-10 18:47:17 -0500

    GET api/tickets/:ticketId

--- api/tickets.js
+++ api/tickets.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const { JWT_SECRET } = process.env;
 const jwt = require("jsonwebtoken");
-const { getAllTickets } = require("../db/tickets");
+const {
+  getAllTickets,
+  getAllUnsoldTickets,
+  getTicketById,
+} = require("../db/tickets");
 const ticketsRouter = express.Router();
 
 ticketsRouter.get("/", async (req, res, next) => {
   try {
-    const tickets = await getAllTickets();
+    const tickets = await getAllUnsoldTickets();
     res.send(tickets);
     return;
-  } catch (error) {
-    next(error);
-    return;
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+ticketsRouter.get("/:ticketId", async (req, res, next) => {
+  const ticketId = req.params.ticketId;
+  try {
+    // check to see if ticket id exists
+    const ticket = await getTicketById(ticketId);
+    if (!ticket) {
+      const err = new Error(`Ticket ${ticketId} not found`);
+      err.status = 400;
+      err.name = "NonExistingTicketError";
+      next(err);
+    }
+    res.send(ticket);
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
