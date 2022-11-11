79818070341a92b24c20dde55275bf55b3667c44
robggarcia <robert.g.garcia82@gmail.com>
2022-11-10 21:05:46 -0500

    api delete ticket order

--- api/tickets_orders.js
+++ api/tickets_orders.js
@@ -7,7 +7,7 @@ const { ArtistExistsError } = require("../errors");
 const { requireUser } = require("./utils");
 const ticketsOrdersRouter = express.Router();
 
-// GET api/tickets_orders/:/ticketOrderId
+// GET api/tickets_orders/:ticketOrderId
 ticketsOrdersRouter.get(
   "/:ticketOrderId",
   requireUser,
@@ -25,6 +25,61 @@ ticketsOrdersRouter.get(
         return;
       }
       res.send(ticketOrder);
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  }
+);
+
+// PATCH api/tickets_orders/:ticketOrderId
+ticketsOrdersRouter.patch(
+  "/:ticketOrderId",
+  requireUser,
+  async (req, res, next) => {
+    const ticketOrderId = req.params.ticketOrderId;
+    const inputFields = req.body;
+    try {
+      // check to see if the artist exists and update if it does
+      const ticketOrder = await getTicketOrderById(+ticketOrderId);
+      if (!ticketOrder) {
+        const err = new Error(
+          `Ticket Order id ${ticketOrderId} does not exist`
+        );
+        err.status = 400;
+        err.name = "NonExistingTicketOrderError";
+        next(err);
+        return;
+      }
+      inputFields.id = ticketOrderId;
+      const updatedTicketOrder = await updatedTicketOrder(inputFields);
+      res.send(updatedTicketOrder);
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  }
+);
+
+// DELETE api/tickets_orders/:ticketOrderId
+ticketsOrdersRouter.delete(
+  "/:ticketOrderId",
+  requireUser,
+  async (req, res, next) => {
+    const ticketOrderId = req.params.ticketOrderId;
+    try {
+      // check to see if the artist exists and update if it does
+      const ticketOrder = await getTicketOrderById(+ticketOrderId);
+      if (!ticketOrder) {
+        const err = new Error(
+          `Ticket Order id ${ticketOrderId} does not exist`
+        );
+        err.status = 400;
+        err.name = "NonExistingTicketOrderError";
+        next(err);
+        return;
+      }
+      // delete the ticket order
+      const deletedTicketOrder = await deletedTicketOrder(+ticketOrderId);
+      res.send(deletedTicketOrder);
     } catch ({ name, message }) {
       next({ name, message });
     }
