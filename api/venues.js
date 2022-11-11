ff63bd9b9f00e18db86be2c7364bd302bdb80479
Eric Sang <eric.sang98@gmail>
2022-11-10 19:43:02 -0500

    eric1

--- api/venues.js
+++ api/venues.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const { JWT_SECRET } = process.env;
 const jwt = require("jsonwebtoken");
+<<<<<<< HEAD
 const { getAllArtists, getArtistById } = require("../db/artists");
 const { ArtistExistsError } = require("../errors");
 const venuesRouter = express.Router();
 
 module.exports = venuesRouter;
+=======
+const { getVenues } = require("../db/venues");
+const venueRouter = express.Router();
+
+venueRouter.get("/", async (req, res, next) => {
+  try {
+    const venues = await getVenues();
+    res.send(venues);
+    return;
+  } catch (error) {
+    next(error);
+    return;
+  }
+});
+>>>>>>> a218a42 (eric)
