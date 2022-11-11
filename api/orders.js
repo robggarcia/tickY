a12a40ecbf19921ba9a8dfe2ef4142dd2c3ec16d
Eric Sang <eric.sang98@gmail>
2022-11-10 21:33:32 -0500

    orders api

--- api/orders.js
+++ api/orders.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { JWT_SECRET } = process.env;
 const jwt = require("jsonwebtoken");
-const { getAllOrders, getOrderById } = require("../db/order");
+const { getAllOrders, getOrderById, updateOrder } = require("../db/order");
 const { requireUser, requireAdmin } = require("./utils");
 const ordersRouter = express.Router();
 
-ordersRouter.get("/", requireUser, requireAdmin, async (req, res, next) => {
+ordersRouter.get("/", requireAdmin, async (req, res, next) => {
   try {
     const orders = await getAllOrders();
     res.send(orders);
@@ -16,26 +16,41 @@ ordersRouter.get("/", requireUser, requireAdmin, async (req, res, next) => {
   }
 });
 
-ordersRouter.get(
-  "/:orderId",
-  requireUser,
-  requireAdmin,
-  async (req, res, next) => {
-    const orderId = req.params.orderId;
-    try {
-      // check to see if order id exists
-      const order = await getOrderById(orderId);
-      if (!order) {
-        const err = new Error(`Order ${orderId} not found`);
-        err.status = 400;
-        err.name = "NonExistingOrderError";
-        next(err);
-      }
-      res.send(order);
-    } catch (error) {
-      next(error);
+ordersRouter.get("/:orderId", requireAdmin, async (req, res, next) => {
+  const orderId = req.params.orderId;
+  try {
+    // check to see if order id exists
+    const order = await getOrderById(orderId);
+    if (!order) {
+      const err = new Error(`Order ${orderId} not found`);
+      err.status = 400;
+      err.name = "NonExistingOrderError";
+      next(err);
     }
+    res.send(order);
+  } catch (error) {
+    next(error);
   }
-);
-
+});
+ordersRouter.patch("/:orderId", requireAdmin, async (req, res, next) => {
+  const orderId = req.params.orderId;
+  const inputFields = req.body;
+  try {
+    // check to see if the order exists and update if it does
+    const checkOrderId = await getOrderById(orderId);
+    if (!checkOrderId) {
+      const err = new Error(`order ${orderId} not found`);
+      err.status = 400;
+      err.name = "NotexistingOrderId";
+      next(err);
+      return;
+    }
+    // update the Order
+    inputFields.id = orderId;
+    const updateOrder = await updateOrder(inputFields);
+    res.send(updateOrder);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 module.exports = ordersRouter;
