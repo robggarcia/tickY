ae0ed1d1c8273c5e31cd6b07cb8cf7fd019d69af
Brandonromero5701 <87501708+Brandonromero5701@users.noreply.github.com>
2022-11-15 19:08:11 -0500

    pulling

--- api/index.js
+++ api/index.js
@@ -73,7 +73,7 @@ apiRouter.use("/tickets_orders", ticketOrdersRouter);
 
 // ROUTER: /api/orders
 const ordersRouter = require("./orders");
-const { getUserById } = require("../db/users");
+
 apiRouter.use("/orders", ordersRouter);
 
 apiRouter.get("*", (req, res) => {
