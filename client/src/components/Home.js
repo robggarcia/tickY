e87c1443875c39fbe26a723962ccb78e94bb1d68
Eric Sang <eric.sang98@gmail>
2022-11-28 23:37:46 -0500

    carosel in progress

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,19 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Carousel } from "react-bootstrap";
+import BootstrapCarousel from "./BootstrapCarousel";
+
 const Home = () => {
   return (
-    <div className="home">
-      <p>Home</p>
-    </div>
+    <>
+      <div className="home">
+        <h1>No Fees. Just Easy Tickets.</h1>
+        <p>let us save you money on sold out shows</p>
+        {/* <BootstrapCarousel /> */}
+      </div>
+    </>
   );
 };
 
