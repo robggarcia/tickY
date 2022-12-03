3cc9a5d713f9fe1111cc7915e2e7633e4e739c0a
robggarcia <robert.g.garcia82@gmail.com>
2022-12-03 13:10:29 -0500

    delpoy test 14

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 
 const client = new Pool(
   process.env.DATABASE_URL
-    ? process.env.DATABASE_URL
+    ? { connectionString: process.env.DATABASE_URL }
     : {
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
