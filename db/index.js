9e38f992170493cf72011486bf90d154968f44bf
robggarcia <robert.g.garcia82@gmail.com>
2022-12-03 12:43:41 -0500

    deploy test 11

--- db/index.js
+++ db/index.js
@@ -4,19 +4,6 @@ const { Pool } = require("pg");
 const client = new Pool(
   process.env.DATABASE_URL
     ? {
-        url: process.env.DATBASE_URL,
-        database: "grace-starter",
-      }
-    : {
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: "grace-starter",
-      }
-);
-
-/* const client = new Pool(
-  process.env.DATABASE_URL
-    ? {
         connectionString: process.env.DATBASE_URL,
         ssl:
           process.env.NODE_ENV === "production"
@@ -28,6 +15,6 @@ const client = new Pool(
         password: process.env.DB_PASSWORD,
         database: "grace-starter",
       }
-); */
+);
 
 module.exports = client;
